a2eae9ecfe76a9d5aed4e0cec92aad05a911540f
arutyunov-a <artash_slavuta@rambler.ru>
2017-08-01 18:08:56 +0300

    feedback-DONE

--- src/js/index.js
+++ src/js/index.js
@@ -35,3 +35,18 @@ $(window).scroll(function() {
     }
 });
 });
+var t;
+function up() {
+    var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+    if(top > 0) {
+        window.scrollBy(0,-100);
+        t = setTimeout('up()',20);
+    } else clearTimeout(t);
+    return false;
+}
+
+$('#scrolltotop').on('click', function(){
+    up();
+    return false;
+})
+
